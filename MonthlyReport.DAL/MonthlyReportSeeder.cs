6e13796427fba2506ec3f0d4071788e4a19dfb98
Paweł Dywan <paweldywan@outlook.com>
2024-11-22 23:07:53 +0100

    Fix

--- MonthlyReport.DAL/MonthlyReportSeeder.cs
+++ MonthlyReport.DAL/MonthlyReportSeeder.cs
@@ -49,80 +49,80 @@ namespace MonthlyReport.DAL
                     new()
                     {
                         Task = "Task 1",
-                        DateFrom = new DateTime(2024, 9, 1, 8, 0, 0),
-                        DateTo = new DateTime(2024, 9, 1, 16, 0, 0),
+                        DateFrom = new DateTime(2024, 9, 1, 8, 0, 0, DateTimeKind.Utc),
+                        DateTo = new DateTime(2024, 9, 1, 16, 0, 0, DateTimeKind.Utc),
                         UserId = userId,
                         Hours = 8
                     },
                     new()
                     {
                         Task = "Task 2",
-                        DateFrom = new DateTime(2024, 9, 2, 8, 0, 0),
-                        DateTo = new DateTime(2024, 9, 2, 16, 0, 0),
+                        DateFrom = new DateTime(2024, 9, 2, 8, 0, 0, DateTimeKind.Utc),
+                        DateTo = new DateTime(2024, 9, 2, 16, 0, 0, DateTimeKind.Utc),
                         UserId = userId,
                         Hours = 8
                     },
                     new()
                     {
                         Task = "Task 3",
-                        DateFrom = new DateTime(2024, 9, 3, 8, 0, 0),
-                        DateTo = new DateTime(2024, 9, 3, 16, 0, 0),
+                        DateFrom = new DateTime(2024, 9, 3, 8, 0, 0, DateTimeKind.Utc),
+                        DateTo = new DateTime(2024, 9, 3, 16, 0, 0, DateTimeKind.Utc),
                         UserId = userId,
                         Hours = 8
                     },
                     new()
                     {
                         Task = "Task 4",
-                        DateFrom = new DateTime(2024, 9, 4, 8, 0, 0),
-                        DateTo = new DateTime(2024, 9, 4, 16, 0, 0),
+                        DateFrom = new DateTime(2024, 9, 4, 8, 0, 0, DateTimeKind.Utc),
+                        DateTo = new DateTime(2024, 9, 4, 16, 0, 0, DateTimeKind.Utc),
                         UserId = userId,
                         Hours = 8
                     },
                     new()
                     {
                         Task = "Task 5",
-                        DateFrom = new DateTime(2024, 9, 5, 8, 0, 0),
-                        DateTo = new DateTime(2024, 9, 5, 16, 0, 0),
+                        DateFrom = new DateTime(2024, 9, 5, 8, 0, 0, DateTimeKind.Utc),
+                        DateTo = new DateTime(2024, 9, 5, 16, 0, 0, DateTimeKind.Utc),
                         UserId = userId,
                         Hours = 8
                     },
                     new()
                     {
                         Task = "Task 6",
-                        DateFrom = new DateTime(2024, 9, 6, 8, 0, 0),
-                        DateTo = new DateTime(2024, 9, 6, 16, 0, 0),
+                        DateFrom = new DateTime(2024, 9, 6, 8, 0, 0, DateTimeKind.Utc),
+                        DateTo = new DateTime(2024, 9, 6, 16, 0, 0, DateTimeKind.Utc),
                         UserId = userId,
                         Hours = 8
                     },
                     new()
                     {
                         Task = "Task 7",
-                        DateFrom = new DateTime(2024, 9, 7, 8, 0, 0),
-                        DateTo = new DateTime(2024, 9, 7, 16, 0, 0),
+                        DateFrom = new DateTime(2024, 9, 7, 8, 0, 0, DateTimeKind.Utc),
+                        DateTo = new DateTime(2024, 9, 7, 16, 0, 0, DateTimeKind.Utc),
                         UserId = userId,
                         Hours = 8
                     },
                     new()
                     {
                         Task = "Task 8",
-                        DateFrom = new DateTime(2024, 9, 8, 8, 0, 0),
-                        DateTo = new DateTime(2024, 9, 8, 16, 0, 0),
+                        DateFrom = new DateTime(2024, 9, 8, 8, 0, 0, DateTimeKind.Utc),
+                        DateTo = new DateTime(2024, 9, 8, 16, 0, 0, DateTimeKind.Utc),
                         UserId = userId,
                         Hours = 8
                     },
                     new()
                     {
                         Task = "Task 9",
-                        DateFrom = new DateTime(2024, 9, 9, 8, 0, 0),
-                        DateTo = new DateTime(2024, 9, 9, 16, 0, 0),
+                        DateFrom = new DateTime(2024, 9, 9, 8, 0, 0, DateTimeKind.Utc),
+                        DateTo = new DateTime(2024, 9, 9, 16, 0, 0, DateTimeKind.Utc),
                         UserId = userId,
                         Hours = 8
                     },
                     new()
                     {
                         Task = "Task 10",
-                        DateFrom = new DateTime(2024, 9, 10, 8, 0, 0),
-                        DateTo = new DateTime(2024, 9, 10, 16, 0, 0),
+                        DateFrom = new DateTime(2024, 9, 10, 8, 0, 0, DateTimeKind.Utc),
+                        DateTo = new DateTime(2024, 9, 10, 16, 0, 0, DateTimeKind.Utc),
                         UserId = userId,
                         Hours = 8
                     }
